a68ae8b854eafaed65346d976b94003153a77406
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-21 15:10:10 +0000

    Add files via upload

--- percent.py
+++ percent.py
@@ -2,12 +2,12 @@ import time
 import os
 
 def draw(a):
-    perc=int(a/2.5)
+    perc=int(a/2)
     aa=str(a)+"%          "
     aa=aa[:4]
     print(aa+"[",end="")
     i="|" * perc
-    ii=(40-len(i))*" "
+    ii=(50-len(i))*" "
     print(i+ii+"]",end="\r")
     
 
