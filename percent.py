ad624d37d2d8f42ffb262b72132cda12211aa05f
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-22 09:58:20 +0000

    Add files via upload

--- percent.py
+++ percent.py
@@ -1,22 +1,12 @@
-import time
-import os
-
-def draw(a):
-    perc=int(a/2)
-    aa=str(a)+"%          "
-    aa=aa[:4]
-    print(aa+"[",end="")
-    i="|" * perc
-    ii=(50-len(i))*" "
-    print(i+ii+"]",end="\r")
-    
+import perc.percent
+import time    
 
 print("\033c\033[40;37m\n")
 counter=0
 while 1:
     if counter > 100:
          break
-    draw(counter)
+    perc.percent.draw(counter)
     counter=counter+4
     time.sleep(1)
 
