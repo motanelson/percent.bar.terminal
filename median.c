05d6922b828f078c4a68e8e26adde46976ba9db7
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-03-01 22:55:31 +0000

    Add files via upload

---
+++ median.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ends -99999999
+void getlist(int *lists,int values){
+    int n=0;
+    for(n=0;n<values;n++)lists[n]=values-n;
+    lists[n]=ends;
+}
+void report(int *lists){
+    int count = 0;
+    printf("[ ");
+    while(lists[count] != ends){
+        printf("%d , ", lists[count]);
+        count++;
+    }
+    printf("]\n");
+}
+int mins(int *lists){
+     int i=0;
+     int count = 0;
+     int total=0;
+     while(lists[count] != ends){
+        if(count==0){
+            total=lists[count];
+
+        }else{
+            if (lists[count]<total)total=lists[count];
+        }
+        count++;
+    
+    }
+    return total;
+}
+
+int maxs(int *lists){
+     int i=0;
+     int count = 0;
+     int total=0;
+     while(lists[count] != ends){
+        if(count==0){
+            total=lists[count];
+
+        }else{
+            if (lists[count]>total)total=lists[count];
+        }
+        count++;
+    
+    }
+    return total;
+}
+
+
+int median(int *lists){
+     int i=0;
+     int count = 0;
+     int total=0;
+     int mn=mins(lists);
+     int mx=maxs(lists);
+     total=mx-mn;
+     total=total/2 + mn; 
+     
+     return total;
+   
+}
+int main(){
+    int i=0;
+    int list1[1000] = { ends};  // 0 termina a lista
+    int list2[1000] = { ends};  // 0 termina a lista
+
+    getlist(list1,20);
+ 
+    printf("\033c\033[40;37m\n");
+    report(list1);
+    i=median(list1);
+    printf("\n%d \n",i);
+    
+    return 0;
+}
